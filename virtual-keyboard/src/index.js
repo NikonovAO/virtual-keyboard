01a600ba7d5bd6cc0a16f4467e8e1754a709eec7
NikonovAO <nikonow@bk.ru>
2022-05-07 00:12:19 +0400

    feat: в проект добавлен webpack, подключен ESLint

---
+++ virtual-keyboard/src/index.js
@@ -0,0 +1,9 @@
+import './style.css'
+import { App } from './App'
+
+new App();
+
+document.addEventListener('keypress', (e)=>{
+    console.log('e.code = ', e.code)
+    console.log('e.key = ', e.key)
+})
